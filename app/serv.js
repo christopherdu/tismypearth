2d344779aceea0f4126334b5dc43b8ddc6563115
Christopher Du <me@christopherdu.com>
2018-09-27 13:04:52 -0400

    API integration done, need to fix bugs

--- app/serv.js
+++ app/serv.js
@@ -3,6 +3,7 @@ const places = require('./places.json');
 const download = require('image-downloader');
 const request = require('request');
 const Twitter = require('twitter');
+const fs = require('fs');
 
 /*
     Call our API endpoints to retrieve data
@@ -21,6 +22,24 @@ function getData(url) {
     });
 }
 
+/* 
+    If something goes wrong with retrieving data, run this function
+*/
+function getDataFallback(url) {
+    return new Promise((resolve, reject) => {
+        request(url[0], (error, response, body) => {
+            if(error) {
+                reject
+            } else {
+                console.log("this is getDataFallback\n");
+                const data = url[1];
+                data["body"] = body;
+                resolve(data);
+            }
+        });
+    })
+}
+
 /*
     Get the desired image URL.
     const getLocation = Get a random sub object from the places JSON object
@@ -31,7 +50,7 @@ function getData(url) {
 function getImgURL() {
     const getLocation = Object.keys(places)[Math.floor(Math.random() * 7)];
     const ran = Math.floor(Math.random() * (places[getLocation].length));
-    const imgURL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6bdb82beef6144523581b81cd724202a&tags=${places[getLocation][ran].name}&content_type=1&safe_search=1&sort=interestingness-desc&license=1&format=json&nojsoncallback=1&per_page=20`;
+    const imgURL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=&tags=${places[getLocation][ran].name}&content_type=1&safe_search=1&sort=interestingness-desc&privacy_filter=1&format=json&nojsoncallback=1&per_page=20`;
     const data = {
         imgURL: imgURL,
         location: places[getLocation][ran]
@@ -50,7 +69,7 @@ function getImgURL() {
 function getImgOwner(data) {
     const userId = data.body.owner;
     return  new Promise((resolve, reject) => {
-        request(`https://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key=6bdb82beef6144523581b81cd724202a&user_id=${userId}&format=json&nojsoncallback=1`, (error, response, body) => {
+        request(`https://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key=&user_id=${userId}&format=json&nojsoncallback=1`, (error, response, body) => {
             if(error) {
                 reject(error);
             } else {
@@ -61,77 +80,128 @@ function getImgOwner(data) {
                 const profile = res.person.profileurl._content;
                 const content = [user, profile];
                 
-                obj["content"] = content;
+                obj["content"] = content
                 resolve(obj);
             }
         })
     })
 }
 
+/* 
+    Download the image to local storage
+*/
+function downloadImg(res) {
+    // Parse the response from the API call into a JSON object
+    const body = JSON.parse(res.body);
+    // Get a random number between 0 and the length of the sub-object within the JSON object
+    const num = Math.floor(Math.random() * body.photos.photo.length);
+    // Set variable to get the content we need
+    const inner = body.photos.photo[num];
+    // console.log(inner);
+    //set body in our object to equal to inner
+    res.body = inner;
+
+    // Download the image to /app/img rename it to img.jpg
+    // Set the options required to download the file
+    const options = {
+        url: `https://farm${inner.farm}.staticflickr.com/${inner.server}/${inner.id}_${inner.secret}_b.jpg`,
+        dest: './app/img/img.jpg'
+    }
+    // Download the actual image
+    download.image(options)
+        .then((filename, image) => {
+            console.log('File saved to', filename);
+        })
+    
+    return new Promise((resolve,reject) => {
+        resolve(res);
+        reject(console.log(err));
+    })
+}
+
+/* 
+    Upload the downloaded image to Twitter services to receive a media_id to attach to the tweet
+*/
 function uploadImg(res) {
+    console.log("This is the uploadImg function -- "+ res);
     const client = new Twitter({
-        consumer_key: 'eX6nSVXpSmejEBWJYGPe4FepT',
-        consumer_secret: 'yFXBv5YxHgcQN91WA5vwA176ptt4mO5b7B0mVcI65ce04g0Vyr',
-        access_token_key: '4429661560-tbobUvkAhbgbxuzr8zwWcwcEeUhj0WfZ8mpSRv7',
-        access_token_secret: 'zrIJnq6fJOhMH6XVdlCRuVS8glplRN1w40UlzkwAhRcQP',
+        consumer_key: '',
+        consumer_secret: '',
+        access_token_key: '',
+        access_token_secret: '',
     });
 
     // Load the image
-    const data = require('fs').readFileSync('app/img/img.jpg');
+    const data = fs.readFileSync('app/img/img.jpg');
 
-    // Make post request on media endpoint. Pass file data as media parameter
-    return new Promise((resolve, reject) => {
-        client.post('media/upload', {media: data}, (error, media, response) => {
-            if (!error) {
-                let obj = res;
-                obj["media_id"] = media.media_id;
-                resolve(obj);
-            } else {
-                reject(error);
-            }
-        });
-    })
-
+    // Make the POST request on media endpoint. Pass file data as media parameter
+    if(fs.existsSync('app/img/img.jpg')) {
+        return new Promise((resolve, reject) => {
+            client.post('media/upload', {media: data}, (error, media, response) => {
+                if (!error) {
+                    const obj = res;
+                    obj["media_id"] = media.media_id_string;
+                    console.log(obj);
+                    resolve(obj);
+                } else {
+                    reject(error);
+                }
+            });
+        })
+    }
 }
 
 function postTweet(data) {
+    console.log("This is the postTweet function -- " + data);
+    const hashloc  = data.location.Country.split(' ').join('');
     
-    client.post('statuses/update', {status:``, media_ids:`${data[3]}`})
+
+    const client = new Twitter({
+        consumer_key: '',
+        consumer_secret: '',
+        access_token_key: '',
+        access_token_secret: '',
+    });
+
+    //📷
+    client.post('statuses/update', {status:`${data.location.name}, ${data.location.Country}\n\n${data.body.title}\n\r📷 by ${data.content[0]}\n\r${data.content[1]}\n\r#earth #travel #photography #${hashloc}`, media_ids:`${data.media_id}`}, (error, tweet, response) => {
+        if(!error) {
+            console.log(tweet);
+        } else {
+            console.log(error);
+        }
+    });
+}
+
+function delImg() {
+    fs.unlink('app/img/img.jpg', err => {
+        if (err){
+            console.log(err);
+        } else {
+            console.log('app/img/img.jpg was deleted!');
+        }
+    })
 }
 
 getImgURL().then(res => {
     // Get the image from the Flickr API
     return getData(res); 
-}).then(res => {
-     // Parse the response from the API call into a JSON object
-     const body = JSON.parse(res.body);
-     // Get a random number between 0 and the length of the sub-object within the JSON object
-     const num = Math.floor(Math.random() * body.photos.photo.length);
-     // Set variable to get the content we need
-     const inner = body.photos.photo[num];
-     //set body in our object to equal to inner
-     res.body = inner;
-     // Download the image to /app/img rename it to img.jpg
-     // Set the options required to download the file
-     const options = {
-         url: `https://farm${inner.farm}.staticflickr.com/${inner.server}/${inner.id}_${inner.secret}_h.jpg`,
-         dest: './app/img/img.jpg'
-     }
+    }).then(res => {
+        return downloadImg(res);
+    }).then(res => {
+        return getImgOwner(res);
+    }).then(res => {
+        return uploadImg(res);
+    }).then(res => {
+        postTweet(res)
+    }).catch(err => console.error(err));
 
-     // Download the actual image
-     download.image(options)
-     .then((filename, image) => {
-         console.log('File saved to', filename);
-     })
-     .catch(err => console.log(err));
-    
-     return getImgOwner(res);
-}).then(res => {
-    console.log(res);
-      return uploadImg(res)
- })//.then(res => {
-//     postTweet(res)
-// }).catch(err => console.error(err));
+    // Delete the image from local storage after 4 seconds
+    // Used to ensure the image is uploaded prior to deletig the image 
+    // and to ensure the right image is uploaded
+    setTimeout(() => {
+        delImg();
+    }, 4000)
 
 
 
